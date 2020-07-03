dbb4339f75943937cc187c831e115f92c7d0e838
456vv <123@456vv.com>
2020-07-03 22:23:11 +0800

    no message

--- test/build.bat
+++ test/build.bat
@@ -9,7 +9,7 @@ set GOARCH=386
 go build -o ./bin/vproxy-linux-386 -ldflags="-s -w" ./vproxy/main.go
 set GOARCH=arm
 set GOARM=7
-go build -o ./bin/vproxy-linux-arm -ldflags="-s -w" ./vproxy/main.go
+go build -o ./bin/vproxy-linux-armv7 -ldflags="-s -w" ./vproxy/main.go
 set GOARCH=arm64
 go build -o ./bin/vproxy-linux-arm64 -ldflags="-s -w" ./vproxy/main.go
 
